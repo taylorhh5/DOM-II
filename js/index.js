abd5b7ef7f5aad5b06edaecbf4d434ff1f1f1745
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-08-06 15:30:20 -0400

    added eventlistener on footer

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+//footer
+
+const foot = document.querySelector('.footer');
+foot.addEventListener('mousemove', function (e) {
+console.log ('footer moused over');
+document.querySelector('.footer').style.backgroundColor= 'yellow';
+})
+
+const foot2 = document.querySelector('.footer');
+foot.addEventListener('mouseout', function (e) {
+console.log ('footer moused out');
+document.querySelector('.footer').style.backgroundColor= 'red';
+})
